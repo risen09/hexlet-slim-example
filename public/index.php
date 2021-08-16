1f06645a4e9adefc6df4ecac7fa87158e99e72e4
Баянов Эльдар <bayanov_m09@mail.ru>
2021-08-16 14:18:24 +0500

    id con

--- public/index.php
+++ public/index.php
@@ -14,4 +14,24 @@ $app->get('/', function ($request, $response) {
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
 });
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+// $app->post('/users', function ($request, $response) {
+//     return $response->withStatus(302);
+// });
+
+$app->post('/users', function ($request, $response) {
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 10);
+    // Тут обработка
+    return $response;
+});
+
 $app->run();
